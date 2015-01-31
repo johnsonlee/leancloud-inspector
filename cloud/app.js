82aab2543852075005baefb4273123370df22be0
johnsonlee <g.johnsonlee@gmail.com>
2015-01-31 11:18:32 +0800

    Commit source code

---
+++ cloud/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var routes = require('cloud/routes');
+var app = express();
+
+app.set('views', 'cloud/views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.cookieSession({secret : 'Inspector'}));
+app.use('/tree', routes.tree);
+
+app.get('/', routes.index);
+
+app.listen();
---
+++ cloud/routes/index.js
@@ -0,0 +1,60 @@
+var fs = require('fs');
+var path = require('path');
+
+(function() {
+
+    this.index = function(req, res) {
+        res.render('index', {});
+    };
+
+    this.tree = function(req, res) {
+        fs.stat(req.path, function(err, stats) {
+            if (err) {
+                res.render('error', {
+                    message : err,
+                });
+            } else if (stats.isDirectory()) {
+                fs.readdir(req.path, function(err, files) {
+                    if (err) {
+                        res.render('error', {
+                            message : err,
+                        });
+                    } else {
+                        if (!/^\/$/.test(req.path)) {
+                            files.unshift('..');
+                        }
+
+                        var data = [];
+                        for (var i = 0; i < files.length; i++) {
+                            var file = files[i];
+
+                            data.push({
+                                name : file,
+                                path : path.join(req.path, file),
+                            });
+                        }
+
+                        res.render('tree', {
+                            data : data,
+                            path : req.path,
+                        });
+                    }
+                });
+            } else if (stats.isFile()) {
+                fs.readFile(req.path, { flag : 'r' }, function(err, data) {
+                    if (err) {
+                        res.render('error', {
+                            message : err,
+                        });
+                    } else {
+                        res.render('raw', {
+                            data : data,
+                            path : req.path,
+                        });
+                    }
+                });
+            }
+        });
+    };
+
+}).call(module.exports);
